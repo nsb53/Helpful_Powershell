eece42cc970b1d4ced3979287a4d15ec3e28b2e0
nsb53 <44839769+nsb53@users.noreply.github.com>
2019-02-19 09:17:29 -0500

    Add files via upload

---
+++ Download_and_Install_SSMS.ps1
@@ -0,0 +1,27 @@
+﻿# Set file and folder path for SSMS installer
+$folderpath="c:\windows\temp"
+$filepath="$folderpath\SSMS-Setup-ENU.exe"
+ 
+#If SSMS not present, download
+if (!(Test-Path $filepath)){
+write-host "Downloading SQL Server SSMS..."
+
+#update to current URL
+$URL="https://go.microsoft.com/fwlink/?linkid=2043154"
+
+$clnt = New-Object System.Net.WebClient
+$clnt.DownloadFile($url,$filepath)
+Write-Host "SSMS installer download complete" -ForegroundColor Green
+ 
+}
+else {
+ 
+write-host "Located the SQL SSMS Installer binaries, moving on to install..."
+}
+ 
+# start the SSMS installer
+write-host "Beginning SSMS install..." -nonewline
+$Parms = " /Install /Passive /Norestart /Logs log.txt"
+$Prms = $Parms.Split(" ")
+& "$filepath" $Prms | Out-Null
+Write-Host "SSMS installation complete" -ForegroundColor Green
